5e7fffd9f8cbe84c93eb89248323a11490812a31
ibrahim mboula <mboulaibrahim@gmail.com>
2023-09-28 00:15:16 +0200

    botom nav-bar ok

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 //pour gerer la navigation entre les pages
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { Ionicons } from '@expo/vector-icons';
+
 
 import { Button, TextInput, StatusBar, StyleSheet, Text, View, KeyboardAvoidingView, Platform } from 'react-native'; 
 import { signUp, login } from './src/services/auth';
@@ -13,12 +17,48 @@ import SignUpScreen from './src/screens/SignUpScreen';
 
 const Stack = createStackNavigator(); //pile de navigation, allez retour entre les pages
 
+const Tab = createBottomTabNavigator();
+
+
+//!=================================================================
 
 //page d'accueil 
 function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Accueil</Text>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>ProfileScreen</Text>
+    </View>
+  );
+}
+
+function AddRecipeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>AddRecipeScreen</Text>
+    </View>
+  );
+}
+
+function FavoritesScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>FavoritesScreen</Text>
+    </View>
+  );
+}
+
+function SearchScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>SearchScreen</Text>
     </View>
   );
 }
@@ -46,7 +86,7 @@ function LoginScreen({ navigation }) {
       alert(result.error);
     } else {
       
-      navigation.navigate('Home');
+      navigation.navigate('Main');
     }
   }
 
@@ -82,18 +122,70 @@ function LoginScreen({ navigation }) {
   );
 }
 
+
+
 export default function App() {
   return (
     <NavigationContainer>
+
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Main" component={MyTabs}  options={{ headerShown: false }}  />
       </Stack.Navigator>
+
     </NavigationContainer>
+
   );
 }
 
+function MyTabs() {
+  return (
+
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      
+      tabBarIcon: ({ color, size }) => {
+        
+        let iconName;
+        let iconSize = size;
+        let iconColor = color;
+
+        if (route.name === 'Home') {
+          iconName = 'home';
+
+        } else if (route.name === 'Profile') {
+          
+          iconName = 'person';
+          
+
+        } else if (route.name === 'New recipe') {
+          iconName = 'create';
+        }else if (route.name === 'Search') {
+          iconName = 'search';
+        }else if (route.name === 'Favorites') {
+          iconName = 'star';
+          iconColor = '#FFDD44';
+        }
+
+        return <Ionicons name={iconName} size={iconSize} color={iconColor} />;
+      },
+    })}
+  >
+
+      <Tab.Screen name="Home" component={HomeScreen} />
+
+      <Tab.Screen name="New recipe" component={AddRecipeScreen} />
+
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      
+      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="Favorites" component={FavoritesScreen} />
+    
+    </Tab.Navigator>
+
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
